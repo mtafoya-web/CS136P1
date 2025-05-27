e3be79e367357ce0ebf102e8feb5a313625703ed
Mario <tafoya1897@gmail.com>
2025-05-27 08:47:18 -0700

    feat: improve input validation and enhance recursive procedures in driver

--- src/driver.cpp
+++ src/driver.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept> // Include for std::invalid_argument
 #include "Factory.h"
 
+//Prints the options for the program will recursively call itself if your option is out of bounds
 int printOptions() {
     int choice = -1;
     std::cout << "\nWelcome to the Matrix Library!" << std::endl;
@@ -15,28 +16,30 @@ int printOptions() {
     std::cout << "5. clone" << std::endl;
     std::cout << "6. Exit " << std::endl;
     std::cin >> choice;
-    while (std::cin.fail() || choice < 0 || choice > 6) {
+    if (std::cin.fail() || choice < 0 || choice > 6) {
         std::cout << "Invalid input. Please enter a number between 1 and 6: " << std::endl;
         std::cin.clear(); // clear the fail state
         std::cin.ignore(); // ignore the rest of the line
-        std::cin >> choice;
+        choice = printOptions();
     }
     return choice;
 }
 
+//Checks if you have entered a positive interger into the rows and columns
 int positiveValidInt(std::string statement) {
     int result = -1;
     std::cout << statement;
     std::cin >> result;
-    while (std::cin.fail() || result <= 0) {
+    if(std::cin.fail() || result <= 0) {
         std::cout << "Invalid input. Please enter a positive integer: " << std::endl;
         std::cin.clear(); // clear the fail state
         std::cin.ignore(); // ignore the rest of the line
-        std::cin >> result;
+        result = positiveValidInt(statement);
     }
     return result;
 }
 
+//Get the Matrix with a string statement
 MatrixLib::Matrix* getMatrix(std::string statement) {
     std::cout << "Enter the dimensions for " << statement << std::endl;
     int rows, cols;
@@ -45,6 +48,7 @@ MatrixLib::Matrix* getMatrix(std::string statement) {
     return MatrixLib::Factory::getPMatrix(rows, cols);
 }
 
+//checks if the dimensions are valid for addition/subtraction/har
 bool validDimensions(const std::unique_ptr<MatrixLib::Matrix>& a, const std::unique_ptr<MatrixLib::Matrix>& b) {
     if (a->rows == b->rows && a->columns == b->columns) {
         return true;
@@ -53,6 +57,7 @@ bool validDimensions(const std::unique_ptr<MatrixLib::Matrix>& a, const std::uni
     return false;
 }
 
+//Checks if you have valid rows and colums for matrix multiplication
 bool validMultDim(const std::unique_ptr<MatrixLib::Matrix>& a, const std::unique_ptr<MatrixLib::Matrix>& b) {
     if (a->columns == b->rows) {
         return true;
@@ -62,8 +67,11 @@ bool validMultDim(const std::unique_ptr<MatrixLib::Matrix>& a, const std::unique
 }
 
 //won't return until you actually input the right values for A and B
-void recursiveCheck(std::unique_ptr<MatrixLib::Matrix> & A, std::unique_ptr<MatrixLib::Matrix> & B){
-    bool check = validDimensions(A, B);
+void recursiveCheck(std::unique_ptr<MatrixLib::Matrix> & A, std::unique_ptr<MatrixLib::Matrix> & B, int choice = 1){
+    bool check;
+    if(choice == 1) check = validDimensions(A, B);
+    else{check = validMultDim(A, B);}
+
     if(check == true){
         return;
     }
@@ -78,52 +86,63 @@ void recursiveCheck(std::unique_ptr<MatrixLib::Matrix> & A, std::unique_ptr<Matr
     
 }
 
-int main() {
-    std::unique_ptr<MatrixLib::Matrix> A(getMatrix("Matrix A"));
+//A procedure to reset the matricies if input is wrong
+void resetProcedure(std::unique_ptr<MatrixLib::Matrix> & A, std::unique_ptr<MatrixLib::Matrix> & B)
+{
+    std::cout << "Creating a new random matrix please choose valid dimensions:" << std::endl;
+    A.reset(getMatrix("Matrix A"));
     std::cout << A << std::endl;
-    std::unique_ptr<MatrixLib::Matrix> B(getMatrix("Matrix B"));
+    B.reset(getMatrix("Matrix B"));
     std::cout << B << std::endl;
+}
 
-    std::unique_ptr<MatrixLib::Matrix> D(MatrixLib::Factory::getPMatrix(3, 3));
-    int endflag = -1;
-    while (endflag < 0) {
-        int choice = printOptions();
-        switch (choice) {
+//Essentially runs the whole program will recursively keep calling itself until you have reached case 6
+void runningProcedure(std::unique_ptr<MatrixLib::Matrix> & A, std::unique_ptr<MatrixLib::Matrix> & B, std::unique_ptr<MatrixLib::Matrix> & D)
+{
+    int choice = printOptions();
+    switch (choice) {
         case 0:
-            std::cout << "Creating a new random matrix please choose valid dimensions:" << std::endl;
-            A.reset(getMatrix("Matrix A"));
-            std::cout << A << std::endl;
-            B.reset(getMatrix("Matrix B"));
-            std::cout << B << std::endl;
+            resetProcedure(A, B);
             break;
         case 1:
-        //Wrap into a function
             recursiveCheck(A, B);
             std::cout << "Matrix A + Matrix B\n" << (*A.get()) + B.get() << std::endl;
+            runningProcedure(A, B, D);
             break;
         case 2:
             recursiveCheck(A, B);
             std::cout << "Matrix A - Matrix B\n" << ((*A.get()) - B.get()) << std::endl;
-            
+            runningProcedure(A, B, D);
             break;
         case 3:
-            recursiveCheck(A, B);
+            recursiveCheck(A, B, choice);
             std::cout << "Matrix A * Matrix B\n" << (*A.get() * B.get()) << std::endl;
-            
+            runningProcedure(A, B, D);
+            break;   
         case 4:
             recursiveCheck(A, B);
             std::cout << "hadamard(Matrix A * Matrix B)\n" << (A->hadamard(B.get())) << std::endl;
+            runningProcedure(A, B, D);
             break;
         case 5:
             D = std::move(A);
             std::cout << "\nAssignment Matrix D = Matrix A: \n" << D << std::endl;
             A = std::move(D);
+            runningProcedure(A, B, D);
             break;
         case 6:
             std::cout << "Exiting the program." << std::endl;
-            endflag = 1;
-            break;
+            return;
         }
-    }
+}
+
+int main() {
+    std::unique_ptr<MatrixLib::Matrix> A(getMatrix("Matrix A"));
+    std::cout << A << std::endl;
+    std::unique_ptr<MatrixLib::Matrix> B(getMatrix("Matrix B"));
+    std::cout << B << std::endl;
+    std::unique_ptr<MatrixLib::Matrix> D(MatrixLib::Factory::getPMatrix(3, 3));
+
+    runningProcedure(A, B, D);
     return 0;
 }
